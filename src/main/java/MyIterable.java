266afcaa64c299de4a7cf73789081de0d22db3f0
dasha-ryzhkova <ryzhkovadasha2002@gmail.com>
2020-11-10 18:35:31 +0200

    fixed

--- src/main/java/MyIterable.java
+++ src/main/java/MyIterable.java
@@ -4,113 +4,16 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class MyIterable {
-
-    //Returns a view of unfiltered containing all elements that satisfy the input predicate retainIfTrue.
-
-
-
     public static  <T> Iterable<T> filter(Iterable<T> unfiltered, Predicate<? super T> retainIfTrue){
-        //Iterable<String> result = Iterable.filter(names, Predicate.containsPattern("a"));
         List<T> res = new LinkedList<>();
         for(T var: unfiltered){
             if(retainIfTrue.test(var)){
                 res.add(var);
             }
         }
-//        Iterator<T> iterable = new Iterator<T>() {
-//            @Override
-//            public Iterator<T> iterator() {
-//                return null;
-//            }
-//        };
         return res;
     }
 
-    /**
-     * Returns a view of {@code unfiltered} containing all elements that satisfy the input predicate
-     * {@code retainIfTrue}. The returned iterable's iterator does not support {@code remove()}.
-     *
-     * <p><b>{@code Stream} equivalent:</b> {@link Stream#filter}.
-     */
-//    public static <T> Iterable<T> filter(
-//            final Iterable<T> unfiltered, final Predicate<? super T> retainIfTrue) {
-//        checkNotNull(unfiltered);
-//        checkNotNull(retainIfTrue);
-//        return new FluentIterable<T>() {
-//            @Override
-//            public Iterator<T> iterator() {
-//                return Iterators.filter(unfiltered.iterator(), retainIfTrue);
-//            }
-//
-//            @Override
-//            public void forEach(Consumer<? super T> action) {
-//                checkNotNull(action);
-//                unfiltered.forEach(
-//                        (T a) -> {
-//                            if (retainIfTrue.test(a)) {
-//                                action.accept(a);
-//                            }
-//                        });
-//            }
-//
-//            @Override
-//            public Spliterator<T> spliterator() {
-//                return CollectSpliterators.filter(unfiltered.spliterator(), retainIfTrue);
-//            }
-//        };
-//    }
-
-
-
-//    public static <F,T> Iterable<T> transform(Iterable<F> fromIterable, Function<? super F,? extends T> function){
-//        Iterable<T> iterable = new Iterable<T>() {
-//            @Override
-//            public Iterator<T> iterator() {
-//                return null;
-//            }
-//        };
-//        return iterable;
-//
-//    }
-
-    /**
-     * Returns a view containing the result of applying {@code function} to each element of {@code
-     * fromIterable}.
-     *
-     * <p>The returned iterable's iterator supports {@code remove()} if {@code fromIterable}'s
-     * iterator does. After a successful {@code remove()} call, {@code fromIterable} no longer
-     * contains the corresponding element.
-     *
-     * <p>If the input {@code Iterable} is known to be a {@code List} or other {@code Collection},
-     * consider {@link Lists#transform} and {@link Collections2#transform}.
-     *
-     * <p><b>{@code Stream} equivalent:</b> {@link Stream#map}
-     */
-//    public static <F, T> Iterable<T> transform(
-//            final Iterable<F> fromIterable, final Function<? super F, ? extends T> function) {
-//        checkNotNull(fromIterable);
-//        checkNotNull(function);
-//        return new FluentIterable<T>() {
-//            @Override
-//            public Iterator<T> iterator() {
-//                return Iterators.transform(fromIterable.iterator(), function);
-//            }
-//
-//            @Override
-//            public void forEach(Consumer<? super T> action) {
-//                checkNotNull(action);
-//                fromIterable.forEach((F f) -> action.accept(function.apply(f)));
-//            }
-//
-//            @Override
-//            public Spliterator<T> spliterator() {
-//                return CollectSpliterators.map(fromIterable.spliterator(), function);
-//            }
-//        };
-//    }
-
-
-    //Returns a view containing the result of applying function to each element of fromIterable.
     public static <F,T> Iterable<T> transform(Iterable<F> fromIterable, Function<? super F,? extends T> function){
         List<T> res = new LinkedList<>();
         for(F var: fromIterable){
--- src/test/java/MyIterableTest.java
+++ src/test/java/MyIterableTest.java
@@ -1,37 +1,87 @@
-import com.google.common.base.Predicate;
+//import com.google.common.base.Predicate;
+//import com.google.common.base.Predicates;
+//import com.google.common.collect.Iterables;
+//import com.google.common.collect.Lists;
 import com.google.common.base.Predicates;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class MyIterableTest {
 
-    private static class TypeA {}
-
-    private interface TypeB {}
-
-    private static class HasBoth extends TypeA implements TypeB {}
-
     @Test
-    public void filter() {
-//        HasBoth hasBoth = new HasBoth();
-//        Iterable<TypeA> alist = Lists.newArrayList(new TypeA(), new TypeA(), hasBoth, new TypeA());
-//        Iterable<TypeB> blist = Iterables.filter(alist, TypeB.class);
-//        assertThat(blist).containsExactly(hasBoth).inOrder();
-        List<String> names = Lists.newArrayList("John", "Jane", "Adam", "Tom");
-        Iterable<String> result = Iterables.filter(names, Predicates.containsPattern("a"));
-
-        //assertEquals(2, result.iterator());
-        System.out.println(result);
-
-
+    public void filterInteger() {
+        List<Integer> list = Lists.newArrayList(1, 2, -3, 4, -5, 6, 7);
+        List<Integer> result = Lists.newArrayList(2, 4, 6, 7);
+        Predicate<Integer> n = num -> num >= 2;
+        Iterable<Integer> list2 = MyIterable.filter(list, n);
+        assertEquals(result, list2);
     }
 
-}
+
+    @Test
+    public void filterFloat() {
+        List<Float> list = Lists.newArrayList(0.2f, 0.6f, 1.2f, 9.12f, 10.12f, 2.1f, 6.1f,1.9f);
+        List<Float> result = Lists.newArrayList(9.12f, 10.12f, 6.1f);
+        Predicate<Float> f = num -> num >= 4;
+        Iterable<Float> list2 = MyIterable.filter(list, f);
+        assertEquals(result, list2);
+
+    }
+    @Test
+    public void filterString() {
+        List<String> list = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        List<String> result = Lists.newArrayList("John", "Tom");
+        Predicate<String> s = str-> str.contains("o");
+        Iterable<String> list2 = MyIterable.filter(list, s);
+        assertEquals(result, list2);
+    }
+
+    @Test
+    public void filterBoolean() {
+        List<Boolean> list = Lists.newArrayList(true, false, true, true ,false);
+        List<Boolean> result = Lists.newArrayList(false, false);
+        Predicate<Boolean> b = bl-> bl == false;
+        Iterable<Boolean> list2 = MyIterable.filter(list, b);
+        assertEquals(result, list2);
+    }
+
+    @Test
+    public void transformInteger() {
+        List<Integer> list = Lists.newArrayList(2, 4, 6, 8);
+        List<Integer> result = Lists.newArrayList(1, 2, 3, 4);
+        Function<Integer,Integer> n = num -> num/2;
+        Iterable<Integer> list2 = MyIterable.transform(list, n);
+        assertEquals(result, list2);
+    }
+    @Test
+    public void transformFloat() {
+        List<Float> list = Lists.newArrayList(0.2f, 0.6f, 1.2f);
+        List<Float> result = Lists.newArrayList(0.4f, 1.2f, 2.4f);
+        Function<Float, Float> f = num -> num*2;
+        Iterable<Float> list2 = MyIterable.transform(list, f);
+        assertEquals(result, list2);
+    }
+    @Test
+    public void transformString() {
+        List<String> list = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        List<String> result = Lists.newArrayList("JOHN", "JANE", "ADAM", "TOM");
+        Function<String, String> s = str-> str.toUpperCase();
+        Iterable<String> list2 = MyIterable.transform(list, s);
+        assertEquals(result, list2);
+    }
+    @Test
+    public void transformBoolean() {
+        List<Boolean> list = Lists.newArrayList(true, false, true, true ,false);
+        List<Boolean> result = Lists.newArrayList(false, true, false, false, true);
+        Function<Boolean, Boolean> b = bl-> !bl;
+        Iterable<Boolean> list2 = MyIterable.transform(list, b);
+        assertEquals(result, list2);
+    }
+
 }
